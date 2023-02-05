8d7feb66be4297029bb6d86fd063cf8c09ce8cc2
siduki <giorgigagnidze@gmail.com>
2023-02-06 02:27:03 +0400

    Add first name and last name input field validation

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+// Input and write same time
+
 function getFirstName() {
   let txt = document.getElementById("firstName");
   let txtValue = txt.value;
@@ -5,8 +7,6 @@ function getFirstName() {
   let result = document.getElementById("fName");
   result.innerText = txtValue;
 }
-
-// Input and write same time
 
 function getLastName() {
   let txt = document.getElementById("lastName");
@@ -53,3 +53,45 @@ input.addEventListener("change", () => {
     display.innerHTML = `<img src=${reader.result} alt="" />`;
   });
 });
+
+// First Name and Last Name validation
+
+let fName = document.getElementById("firstName");
+
+function validateFirstName() {
+  let name = document.getElementById("firstName").value;
+  let novalid = document.getElementById("novalid-01");
+  let letters = /^[ა-ჰ]+$/;
+
+  if (name.length > 1 && name.match(letters)) {
+    fName.style.borderColor = "#98E37E";
+    fName.classList.add("input-background");
+    novalid.style.display = "none";
+    return false;
+  } else {
+    fName.style.borderColor = "#EF5050";
+    fName.classList.remove("input-background");
+    novalid.style.display = "block";
+    return true;
+  }
+}
+
+let lName = document.getElementById("lastName");
+
+function validateLastName() {
+  let name = document.getElementById("lastName").value;
+  let novalid = document.getElementById("novalid-02");
+  let letters = /^[ა-ჰ]+$/;
+
+  if (name.length > 1 && name.match(letters)) {
+    lName.style.borderColor = "#98E37E";
+    lName.classList.add("input-background");
+    novalid.style.display = "none";
+    return false;
+  } else {
+    lName.style.borderColor = "#EF5050";
+    lName.classList.remove("input-background");
+    novalid.style.display = "block";
+    return true;
+  }
+}
